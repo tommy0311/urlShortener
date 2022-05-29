ecd8d6e710382306d24db86fef82a4c000624b7a
litangchen <chen.li.tang@gmail.com>
2022-05-29 17:21:55 +0800

    feat: connect to mongodb

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require("express-handlebars");
 const bodyParser = require('body-parser')
 
 const routes = require('./routes') // It is equal to require('./routes/index')
+require('./config/mongoose')
 
 const app = express();
 const port = 3000;
